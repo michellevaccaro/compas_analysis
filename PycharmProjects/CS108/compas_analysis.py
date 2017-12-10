36f6b498651f4643d2d3bdfa58f77910cf7d5bd2
Michelle Vaccaro <michelle_vaccaro@college.harvard.edu>
2017-12-09 21:50:00 -0500

    Testing works

--- PycharmProjects/CS108/compas_analysis.py
+++ PycharmProjects/CS108/compas_analysis.py
@@ -3,13 +3,18 @@ import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
 
-X = []
-two_year_recid = []
+training_data = []
+testing_data = []
+training_label = []
+testing_label = []
 
-def build_logistic_model(fname):
+
+def load_file(fname):
     with open(fname, "rb") as f:
         reader = csv.DictReader(f)
 
+        arr = []
+        labels = []
         # read in values from file
         for row in reader:
             # c_charge_degree
@@ -46,64 +51,51 @@ def build_logistic_model(fname):
             if row['sex'] == 'Female':
                 sex = 1
 
-            X.append([c_charge_degree, race, age_cat, sex, int(row['priors_count'])])
-            two_year_recid.append(row['two_year_recid'])
+            arr.append([c_charge_degree, race, age_cat, sex, int(row['priors_count'])])
+            labels.append(row['two_year_recid'])
 
+    return arr, labels
+
+
+def build_logistic_model(fname):
     clf = LogisticRegression()
-    clf.fit(X, two_year_recid)
-    print('Coefficients:  ', clf.coef_)
+    clf.fit(training_data, training_label)
+    print 'Built Logistic Regression'
+    # print clf.coef_
+    return clf
 
 
 def build_mlp_model(fname):
-    with open(fname, "rb") as f:
-        reader = csv.DictReader(f)
-
-        # read in values from file
-        for row in reader:
-            # c_charge_degree
-            if row['c_charge_degree'] == 'M':
-                c_charge_degree = 0
-            if row['c_charge_degree'] == 'F':
-                c_charge_degree = 1
-
-            # race
-            if row['race'] == 'Caucasian':
-                race = 0
-            if row['race'] == 'African-American':
-                race = 1
-            if row['race'] == 'Hispanic':
-                race = 2
-            if row['race'] == 'Asian':
-                race = 3
-            if row['race'] == 'Native American':
-                race = 4
-            if row['race'] == 'Other':
-                race = 5
-
-            # age_cat
-            if row['age_cat'] == 'Less than 25':
-                age_cat = 0
-            if row['age_cat'] == '25 - 45':
-                age_cat = 1
-            if row['age_cat'] == 'Greater than 45':
-                age_cat = 2
-
-            # sex
-            if row['sex'] == 'Male':
-                sex = 0
-            if row['sex'] == 'Female':
-                sex = 1
-
-            X.append([c_charge_degree, race, age_cat, sex, int(row['priors_count'])])
-            two_year_recid.append(row['two_year_recid'])
-
     clf = MLPClassifier(
         hidden_layer_sizes=(100,), activation='relu', solver='adam', alpha=0.001, batch_size='auto',
         learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=1000, shuffle=True,
         random_state=0, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True,
         early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-    nn_mlp = clf.fit(X, two_year_recid)
-    print clf.predict([[0, 0, 2, 0, 1]])
+    nn_mlp = clf.fit(training_data, training_label)
+    print "Built MLP"
+    return nn_mlp
 
-build_logistic_model('cleaned_COMPAS_data.csv')
-build_mlp_model('cleaned_COMPAS_data.csv')
+
+def test_model(model):
+    if model == 'nn_mlp':
+        clf = build_mlp_model('COMPAS_testing_data')
+    elif model == 'log_model':
+        clf = build_logistic_model('COMPAS_testing_data')
+    else:
+        print 'Invalid input'
+
+    num_correct = 0
+    for i in range(len(testing_data)):
+        if(int(clf.predict([testing_data[i]])[0]) == int(testing_label[i])):
+            num_correct += 1
+
+    print num_correct/float(len(testing_data))
+
+
+training_data, training_label = load_file('COMPAS_training_data.csv')
+# log = build_logistic_model('cleaned_COMPAS_data.csv')
+# nn = build_mlp_model('cleaned_COMPAS_data.csv')
+
+testing_data, testing_label = load_file('COMPAS_testing_data.csv')
+test_model('nn_mlp')
+test_model('log_model')
